13fc2896c9a9ad66cc54c8e64f6f9c5bddd36525
Valeria Lopez <val.lopez1011@gmail.com>
2021-01-12 19:11:37 -0500

    Adds OT pay calculation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function calculateCheck() {
   const casesCom = document.getElementById("cases-com").value;
   const breederTrans = document.getElementById("breeder-transfer").value;
   const downtime = document.getElementById("downtime").value;
+  const otHours = document.getElementById("ot-hours").value;
 
   const SEMI_MILES_PRICE = 0.3135;
   const DOLLIES_PRICE = 4.4044;
@@ -16,12 +17,20 @@ function calculateCheck() {
   const totalCasesCom = casesCom * CASES_COM_PRICE;
   const totalBreederTrans = breederTrans * BREEDER_TRANS_PRICE;
   const totalDowntime = downtime * DOWNTIME_PRICE;
+  var totalPay = totalMiles + totalDollies + totalCasesCom +
+    totalBreederTrans + totalDowntime;
 
-  const totalPay = (totalMiles + totalDollies + totalCasesCom +
-    totalBreederTrans + totalDowntime).toFixed(2);
+  const approxOTPayRate = 0.00614 * totalPay + 2.16399;
+  const totalOT = approxOTPayRate * otHours;
+
+  console.log(approxOTPayRate);
+  console.log(totalOT);
+
+  totalPay = (totalPay + totalOT).toFixed(2);
+  console.log(totalPay);
 
   const calcPara = document.createElement("p");
-  const calcText = document.createTextNode("Gross check: $" + totalPay);
+  const calcText = document.createTextNode("Approx. gross check: $" + totalPay);
   calcPara.appendChild(calcText);
 
   const calcDiv = document.getElementById("final-check");
