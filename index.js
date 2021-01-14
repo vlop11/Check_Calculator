bdcc3546e74451fda57694237f26a1ec82700b91
Valeria Lopez <val.lopez1011@gmail.com>
2021-01-13 19:17:04 -0500

    Updated OT rate calculation

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
+const SEMI_MILES_PAY = 0.3135;
+const DOLLIES_PAY = 4.4044;
+const CASES_PAY = 0.5254;
+const BREEDER_TRANS_PAY = 25.00;
+const DOWNTIME_PAY = 13.90;
+
 function calculateCheck() {
+  const hours = document.getElementById("hours").value;
   const miles = document.getElementById("miles").value;
   const dollies = document.getElementById("dollies").value;
-  const casesCom = document.getElementById("cases-com").value;
+  const cases = document.getElementById("cases").value;
   const breederTrans = document.getElementById("breeder-transfer").value;
   const downtime = document.getElementById("downtime").value;
-  const otHours = document.getElementById("ot-hours").value;
 
-  const SEMI_MILES_PRICE = 0.3135;
-  const DOLLIES_PRICE = 4.4044;
-  const CASES_COM_PRICE = 0.5254;
-  const BREEDER_TRANS_PRICE = 20.00;
-  const DOWNTIME_PRICE = 13.90;
+  const totalMilesPay = miles * SEMI_MILES_PAY;
+  const totalDolliesPay = dollies * DOLLIES_PAY;
+  const totalCasesPay = cases * CASES_PAY;
+  const totalBreederTransPay = breederTrans * BREEDER_TRANS_PAY;
+  const totalDowntimePay = downtime * DOWNTIME_PAY;
+  var totalPay = totalMilesPay + totalDolliesPay + totalCasesPay +
+    totalBreederTransPay + totalDowntimePay;
 
-  const totalMiles = miles * SEMI_MILES_PRICE;
-  const totalDollies = dollies * DOLLIES_PRICE;
-  const totalCasesCom = casesCom * CASES_COM_PRICE;
-  const totalBreederTrans = breederTrans * BREEDER_TRANS_PRICE;
-  const totalDowntime = downtime * DOWNTIME_PRICE;
-  var totalPay = totalMiles + totalDollies + totalCasesCom +
-    totalBreederTrans + totalDowntime;
+if (hours != 0 && hours > 40) {
+  const hoursNoDT = hours - downtime;
+  const dolliesPerHour = dollies/hoursNoDT;
 
-  const approxOTPayRate = 0.00614 * totalPay + 2.16399;
-  const totalOT = approxOTPayRate * otHours;
+  const approxOTPayRate = 1.38743 * dolliesPerHour + 5.85692;
+  const totalOT = approxOTPayRate * (hours - 40);
 
-  console.log(approxOTPayRate);
-  console.log(totalOT);
-
-  totalPay = (totalPay + totalOT).toFixed(2);
-  console.log(totalPay);
+  totalPay = totalPay + totalOT;
+}
 
   const calcPara = document.createElement("p");
-  const calcText = document.createTextNode("Approx. gross check: $" + totalPay);
+  const calcText = document.createTextNode("Approx. gross check: $" +
+    totalPay.toFixed(2));
   calcPara.appendChild(calcText);
 
   const calcDiv = document.getElementById("final-check");
